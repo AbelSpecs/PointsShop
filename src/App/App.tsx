f212db45ec2ca788775d4703f6e7bee6ba59306e
Abel Colina <abdv.vargas@gmail.com>
2023-06-02 16:12:23 -0400

    wip: pointsShop

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,16 +3,20 @@ import * as React from "react";
 import logo from "../assets/logo.svg";
 
 import styles from "./App.module.scss";
+import AeroNavbar from "../components/aero-navbar/aero-navbar";
+import Dock from "~/components/dock/dock";
 
 const App: React.FC = () => {
   return (
     <main className={styles.container}>
-      <header className={styles.header}>
+      <AeroNavbar/>
+      <Dock/>
+      {/* <header className={styles.header}>
         <h1>
           <img alt="Aerolab" src={logo} width={128} />
         </h1>
         <h3>Lets get this party started</h3>
-      </header>
+      </header> */}
     </main>
   );
 };
